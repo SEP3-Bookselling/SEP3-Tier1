a1425fcd33db2a08b0d4b99382a986cdb6eb8767
magicaltermite <293106@VIAUC.dk>
2020-11-04 09:52:59 +0100

    Added services

---
+++ SEP3-Tier1/Data/ISaleService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SEP3_Tier1.Models;
+
+namespace SEP3_Tier1.Data
+{
+    public interface ISaleService
+    {
+        Task<IList<BookSale>> GetSaleAsync();
+        Task AddSaleAsync();
+        Task RemoveSaleAsync();
+        Task UpdateAsync();
+    }
+}
---
+++ SEP3-Tier1/Data/SaleService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using SEP3_Tier1.Models;
+
+namespace SEP3_Tier1.Data
+{
+    public class SaleService : ISaleService
+    {
+
+        private string uri = "https://localhost:5003";
+        private HttpClient client;
+
+
+        public SaleService() {
+            client = new HttpClient();
+        }
+        
+        
+        
+        public Task<IList<BookSale>> GetSaleAsync() {
+            Task<string> stringAsync = client.GetStringAsync(uri + "/sales");
+        }
+
+        public Task AddSaleAsync() {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveSaleAsync() {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
