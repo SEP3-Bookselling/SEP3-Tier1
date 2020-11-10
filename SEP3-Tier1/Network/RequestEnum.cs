0d84d8e33015f2e2e2879418c864dbc54c06d28b
AnneLindberg <aans@live.dk>
2020-11-10 14:11:24 +0100

    added networkfolder with enums and request class

---
+++ SEP3-Tier1/Network/RequestEnum.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SEP3_Tier1.Network
+{
+    [Newtonsoft.Json.JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RequestEnum
+    {
+        recieveProofOfConcept,
+        sendProofOfConcept
+    }
+}
