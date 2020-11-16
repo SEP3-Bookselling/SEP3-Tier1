139982659472dff8319278eb1344fecaad03c07a
magicaltermite <293106@VIAUC.dk>
2020-11-16 13:08:13 +0100

    Created new class for login

---
+++ SEP3-Tier1/Authentication/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace SEP3_Tier1.Authentication
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync() {
+            throw new System.NotImplementedException();
+            string commas = ",,,";
+
+            commas.TrimEnd(',');
+        }
+    }
+}
