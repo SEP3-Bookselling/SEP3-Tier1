f05564964e64b023f1bdc68d0cfed2a7451f4a9e
Markus Lorensen <293106@VIAUC.dk>
2020-12-10 09:33:50 +0100

    Removed writelines

--- SEP3-Tier1/Data/BookSale/PurchaseService.cs
+++ SEP3-Tier1/Data/BookSale/PurchaseService.cs
@@ -36,11 +36,9 @@ namespace SEP3_Tier1.Data
         }
 
         public async Task<IList<PurchaseRequest>> GetPurchaseRequestAsync(string username) {
-            Console.WriteLine("username in get purchase in service: " + username);
             Task<string> stringAsync = client.GetStringAsync(uri + $"/Purchase?username={username}");
             string message = await stringAsync;
             List<PurchaseRequest> result = JsonSerializer.Deserialize<List<PurchaseRequest>>(message);
-            Console.WriteLine("Get purchase request in PurchaseService: " + message);
             return result;
         }
 
